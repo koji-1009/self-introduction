a9963095cbb5e3e5304f5ed405f3c006bde47198
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-05-07 00:19:04 +0900

    Add TestConponents

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import TestComponent from './components/TestComponent'
+
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
 import withStyles, { WithStyles, StyleRules } from "@material-ui/core/styles/withStyles";
 import createStyles from "@material-ui/core/styles/createStyles";
@@ -11,21 +13,8 @@ type Props = WithStyles<typeof styles>;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <TestComponent title="React TypeScript Material-UI Example" />
     </div>
   );
 }
---
+++ app/src/components/TestComponent.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import Button from '@material-ui/core/Button';
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import withStyles, { WithStyles, StyleRules } from "@material-ui/core/styles/withStyles";
+import createStyles from "@material-ui/core/styles/createStyles";
+
+const styles = (theme: Theme): StyleRules =>
+    createStyles({
+        root: {
+            textAlign: "center"
+        },
+        header: {
+            backgroundColor: theme.palette.primary.main,
+            boxShadow: theme.shadows[2],
+            padding: theme.spacing.unit * 2,
+        },
+        counter: {
+            fontSize: 60
+        },
+    });
+
+interface Props extends WithStyles<typeof styles> {
+    title?: string;
+}
+
+interface State {
+    readonly counter: number;
+}
+
+class TestComponent extends React.Component<Props, State> {
+    readonly state: State = {
+        counter: 0
+    };
+
+    private _onIncrement = () => {
+        this.setState(prevState => ({
+            counter: prevState.counter + 1
+        }));
+    };
+
+    public render() {
+        const { classes, title } = this.props;
+        const { counter } = this.state;
+        return (
+            <div className={classes.root}>
+                <div className={classes.header}>
+                    {title || "My Third TS Component"}
+                </div>
+                <div>
+                    <span className={classes.counter}>{counter}</span>
+                </div>
+                <div>
+                    <Button variant="contained" color="primary" onClick={this._onIncrement}>
+                        Increment
+          </Button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles)(TestComponent);
