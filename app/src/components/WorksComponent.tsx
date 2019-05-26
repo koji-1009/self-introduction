0583a3363fb6fbdabfeff08283c99f8a4c9e1208
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-05-26 22:37:32 +0900

    Moidfy target and rel at a tag

--- app/src/components/WorksComponent.tsx
+++ app/src/components/WorksComponent.tsx
@@ -17,21 +17,21 @@ class WorkComponent extends React.Component<WorkProps> {
         return (
             <div className={classes.root}>
                 <ul>
-                    <li>Play Stored Application <a href="https://play.google.com/store/apps/developer?id=Koji+Wakamiya&hl=ja">Store</a>
+                    <li>Play Stored Application <a href="https://play.google.com/store/apps/developer?id=Koji+Wakamiya&hl=ja" target="_blank" rel="noopener noreferrer">Store</a>
                         <ul>
-                            <li><a href="https://play.google.com/store/apps/details?id=com.app.dr1009.qrshot">QRShot</a></li>
-                            <li><a href="https://play.google.com/store/apps/details?id=com.app.dr1009.dailytodo">DailyToDo</a></li>
-                            <li><a href="https://play.google.com/store/apps/details?id=com.app.dr1009.addbu">LocationViewer</a></li>
-                            <li><a href="https://play.google.com/store/apps/details?id=com.sf_lolitahag.kitsunehomerun">きつねホームラン</a></li>
-                            <li><a href="https://play.google.com/store/apps/details?id=com.app.dr1009.webviewchecker">WebviewChecker</a></li>
+                            <li><a href="https://play.google.com/store/apps/details?id=com.app.dr1009.qrshot" target="_blank" rel="noopener noreferrer">QRShot</a></li>
+                            <li><a href="https://play.google.com/store/apps/details?id=com.app.dr1009.dailytodo" target="_blank" rel="noopener noreferrer">DailyToDo</a></li>
+                            <li><a href="https://play.google.com/store/apps/details?id=com.app.dr1009.addbu" target="_blank" rel="noopener noreferrer">LocationViewer</a></li>
+                            <li><a href="https://play.google.com/store/apps/details?id=com.sf_lolitahag.kitsunehomerun" target="_blank" rel="noopener noreferrer">きつねホームラン</a></li>
+                            <li><a href="https://play.google.com/store/apps/details?id=com.app.dr1009.webviewchecker" target="_blank" rel="noopener noreferrer">WebviewChecker</a></li>
                         </ul>
                     </li>
                     <li>Android Library
                         <ul>
-                            <li><a href="https://github.com/koji-1009/EmptyRecyclerView">EmptyRecyclerView</a></li>
-                            <li><a href="https://github.com/koji-1009/FailureStatus">FailureStatus</a></li>
-                            <li><a href="https://github.com/koji-1009/ChronoDialogPreference">ChronoDialogPreference</a></li>
-                            <li><a href="https://github.com/koji-1009/EasyNotificationChannel">EasyNotificationChannel</a></li>
+                            <li><a href="https://github.com/koji-1009/EmptyRecyclerView" target="_blank" rel="noopener noreferrer">EmptyRecyclerView</a></li>
+                            <li><a href="https://github.com/koji-1009/FailureStatus" target="_blank" rel="noopener noreferrer">FailureStatus</a></li>
+                            <li><a href="https://github.com/koji-1009/ChronoDialogPreference" target="_blank" rel="noopener noreferrer">ChronoDialogPreference</a></li>
+                            <li><a href="https://github.com/koji-1009/EasyNotificationChannel" target="_blank" rel="noopener noreferrer">EasyNotificationChannel</a></li>
                         </ul>
                     </li>
                 </ul>
