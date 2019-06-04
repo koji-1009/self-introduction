df0e07edcedd1ccd3ff3325763768cca2a44f12d
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-06-05 00:17:27 +0900

    Updates

---
+++ precache-manifest.46130bd0b2ef430e57025dfab36ae3b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc60d0815233984f99e32deeffa0bd8",
+    "url": "/self-introduction-react/index.html"
+  },
+  {
+    "revision": "0d3c362f0fc8fb4bf51c",
+    "url": "/self-introduction-react/static/css/main.3e545749.chunk.css"
+  },
+  {
+    "revision": "bff46bb672d80f41b3dd",
+    "url": "/self-introduction-react/static/js/2.8c92ac81.chunk.js"
+  },
+  {
+    "revision": "0d3c362f0fc8fb4bf51c",
+    "url": "/self-introduction-react/static/js/main.a696bfd3.chunk.js"
+  },
+  {
+    "revision": "34c113adbe9ec81e82b8",
+    "url": "/self-introduction-react/static/js/runtime~main.05c3a597.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/self-introduction-react/precache-manifest.d8e424141c7533e596b77e1dcca716c7.js"
+  "/self-introduction-react/precache-manifest.46130bd0b2ef430e57025dfab36ae3b7.js"
 );
 
 self.addEventListener('message', (event) => {
