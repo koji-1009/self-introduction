6785e780cdf9d8f2d0ad9595b05c7e2e7b50967b
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-06-05 09:24:06 +0900

    Add my profile

--- app/src/components/ProfileComponent.tsx
+++ app/src/components/ProfileComponent.tsx
@@ -14,7 +14,13 @@ class ProfileComponent extends React.Component<Props> {
 
     render() {
         return <div>
+            <p>Hi! I'm Koji-1009, Android Application Developer!</p>
 
+            <h2>links</h2>
+                <ul>
+                    <li><a href="https://github.com/koji-1009" target="_blank" rel="noopener noreferrer">Github</a></li>
+                    <li><a href="https://dr1009.hatenablog.com/" target="_blank" rel="noopener noreferrer">Blog(in Japanease)</a></li>
+                </ul>
         </div>
     }
 }
