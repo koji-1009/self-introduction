7e6897d18d7a59fca45428183169ee3926630017
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-07-13 01:16:33 +0900

    Updates

--- precache-manifest.34aaa90817dfb0d29e59862470fee04e.js
+++ precache-manifest.34aaa90817dfb0d29e59862470fee04e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "abe39e766b1786837931a3f9d5b8cc29",
+    "revision": "e9611a2cbc5445d7e8bc7100687f3720",
     "url": "/self-introduction-react/index.html"
   },
   {
-    "revision": "34561ccf063b8b32ed8e",
+    "revision": "874e13dd306bd365adc3",
     "url": "/self-introduction-react/static/css/main.3e545749.chunk.css"
   },
   {
-    "revision": "7787a69a95ff09fdf91c",
-    "url": "/self-introduction-react/static/js/2.0a119d57.chunk.js"
+    "revision": "a57be6f843e752b96501",
+    "url": "/self-introduction-react/static/js/2.906cb66c.chunk.js"
   },
   {
-    "revision": "34561ccf063b8b32ed8e",
-    "url": "/self-introduction-react/static/js/main.74bbe3d5.chunk.js"
+    "revision": "874e13dd306bd365adc3",
+    "url": "/self-introduction-react/static/js/main.4d85266b.chunk.js"
   },
   {
     "revision": "34c113adbe9ec81e82b8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/self-introduction-react/precache-manifest.f1fd82f79a7afe16419107b1a1b6daf1.js"
+  "/self-introduction-react/precache-manifest.34aaa90817dfb0d29e59862470fee04e.js"
 );
 
 self.addEventListener('message', (event) => {
