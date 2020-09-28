fe1775c5463ae898b72ca028fa907c2cba4b816d
koji-1009 <koji-1009@users.noreply.github.com>
2020-09-28 22:48:31 +0000

    deploy: 1f9fba8ae7fd259e7262a693e233b180babef519

---
+++ precache-manifest.f363664d2e9537e4ef07a991992192fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8412532a3bfaf5012704e4c24857356",
+    "url": "/self-introduction-react/index.html"
+  },
+  {
+    "revision": "374336b94992a3a4b480",
+    "url": "/self-introduction-react/static/js/2.7dacd0c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/self-introduction-react/static/js/2.7dacd0c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02aebae793258acbfbe1",
+    "url": "/self-introduction-react/static/js/main.65f8f455.chunk.js"
+  },
+  {
+    "revision": "58215b7d6cad04e1a614",
+    "url": "/self-introduction-react/static/js/runtime-main.810fb4c4.js"
+  }
+]);
