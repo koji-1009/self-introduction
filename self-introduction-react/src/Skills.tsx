f24722ed0347a0924b50f5a34b1b224c135ec04a
Koji Wakamiya <koji.wakamiya@gmail.com>
2021-02-04 22:54:42 +0900

    Update profile

--- self-introduction-react/src/Skills.tsx
+++ self-introduction-react/src/Skills.tsx
@@ -8,18 +8,65 @@ export default function Skills() {
             <Typography variant="h4" component="h2" gutterBottom>Skills</Typography>
             <ExpansionPanel>
                 <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header" >
+                    <Typography>Flutter Application</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <List>
+                        <ListItem>
+                            <Grid container direction="column" justify="center" spacing={1}>
+                                <Typography variant="subtitle1">Skills</Typography>
+                                <ListItem>
+                                    <ListItemText primary="RxStream" />
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText primary="RiverPod/Provider" />
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText primary="Freezed + StateNotifier" />
+                                </ListItem>
+                            </Grid>
+                        </ListItem>
+                        <ListItem>
+                            <Grid container direction="column" justify="center" spacing={1}>
+                                <Typography variant="subtitle1">Application</Typography>
+                                <Grid item>
+                                    <Link href="https://github.com/koji-1009/color_bg">color_bg</Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="https://github.com/koji-1009/flutter_web_deploy">flutter_web_deploy</Link>
+                                </Grid>
+                            </Grid>
+                        </ListItem>
+                        <ListItem>
+                            <Grid container direction="column" justify="center" spacing={1}>
+                                <Typography variant="subtitle1">Library</Typography>
+                                <Grid item>
+                                    <Link href="https://github.com/koji-1009/flutter_auth_ui">flutter_auth_ui</Link>
+                                </Grid>
+                            </Grid>
+                        </ListItem>
+                    </List>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header" >
                     <Typography>Android Application</Typography>
                 </ExpansionPanelSummary>
                 <ExpansionPanelDetails>
                     <List>
                         <ListItem>
-                            <ListItemText primary="Java 7, 8 + RxJava" />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="Kotlin + Kotlin Coroutines" />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="Flutter" />
+                            <Grid container direction="column" justify="center" spacing={1}>
+                                <Typography variant="subtitle1">Skills</Typography>
+                                <ListItem>
+                                    <ListItemText primary="Java 7, 8 + RxJava" />
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText primary="Kotlin + Kotlin Coroutines" />
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemText primary="Dagger 2" />
+                                </ListItem>
+                            </Grid>
                         </ListItem>
                         <ListItem>
                             <Grid container direction="column" justify="center" spacing={1}>
@@ -28,13 +75,16 @@ export default function Skills() {
                                     <Link href="https://github.com/koji-1009/EmptyRecyclerView">EmptyRecyclerView</Link>
                                 </Grid>
                                 <Grid item>
-                                    <Link href="https://github.com/koji-1009/EasySettingsPanel">EasySettingsPanel</Link>ï
+                                    <Link href="https://github.com/koji-1009/EasySettingsPanel">EasySettingsPanel</Link>
                                 </Grid>
                                 <Grid item>
                                     <Link href="https://github.com/koji-1009/ChronoDialogPreference">ChronoDialogPreference</Link>
                                 </Grid>
                                 <Grid item>
                                     <Link href="https://github.com/koji-1009/FailureStatus">FailureStatus</Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="https://github.com/koji-1009/dangerjs-android-plugin">dangerjs-android-plugin</Link>
                                 </Grid>
                             </Grid>
                         </ListItem>
@@ -62,39 +112,18 @@ export default function Skills() {
                 <ExpansionPanelDetails>
                     <List>
                         <ListItem>
-                            <ListItemText primary="Swift" />
+                            <Grid container direction="column" justify="center" spacing={1}>
+                                <Typography variant="subtitle1">Skills</Typography>
+                                <Grid item>
+                                    <ListItemText primary="Swift" />
+                                </Grid>
+                            </Grid>
                         </ListItem>
                         <ListItem>
                             <Grid container direction="column" justify="center" spacing={1}>
                                 <Typography variant="subtitle1">Apps</Typography>
                                 <Grid item>
                                     <Typography>Kaede50(closed)</Typography>
-                                </Grid>
-                            </Grid>
-                        </ListItem>
-                    </List>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-            <ExpansionPanel>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header" >
-                    <Typography>Web</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <List>
-                        <ListItem>
-                            <ListItemText primary="React + TypeScript" />
-                        </ListItem>
-                        <ListItem>
-                            <ListItemText primary="Flutter" />
-                        </ListItem>
-                        <ListItem>
-                            <Grid container direction="column" justify="center" spacing={1}>
-                                <Typography variant="subtitle1">Apps</Typography>
-                                <Grid item>
-                                    <Typography>Self Introduction(This page)</Typography>
-                                </Grid>
-                                <Grid item>
-                                    <Link href="https://flutter-web-deploy-demo.firebaseapp.com">Flutter web deploy demo</Link>
                                 </Grid>
                             </Grid>
                         </ListItem>
--- self-introduction-react/src/WhoAmI.tsx
+++ self-introduction-react/src/WhoAmI.tsx
@@ -9,7 +9,7 @@ export default function WhoAmI() {
         <ListItemAvatar>
           <Avatar alt="koji-1009" src="https://avatars1.githubusercontent.com/u/17231507" />
         </ListItemAvatar>
-        <ListItemText primary="Koji Wakamiya" secondary="Android Application Developer" />
+        <ListItemText primary="Koji Wakamiya" secondary="Mobile Application Developer" />
       </ListItem>
     </List>
   );
